e49365049ad71af1c387d84a2503d8dbe25c2295
Carrie G <carr1eg@Hotmail.com>
2021-08-27 03:31:00 +0000

    use callback to calc iss flyover time

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 // index.js
 const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
+const nextISSTimesForMyLocation = function(cb) {
+ fetchMyIP((error, ip) => {
+   if (error) {
+    return cb(error, null);
+   }
+   fetchCoordsByIP(ip, (err, data) => {
+    if (err) {
+      return cb(error, null);
+    }
+    fetchISSFlyOverTimes(data, (err, data) => {
+      if (err) {
+        return cb(error, null);
+      }
+      return cb(null, data);
+    });
+  })
+ })
+}
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  for(time of passTimes) {
+    const day = new Date(time['risetime'] * 1000).toLocaleString("en-US");
+    console.log(`Next pass at ${day} (Pacific Daylight Time) for ${time.duration} seconds!`);
+  }
+});
+
+
+
+
+
+
 // fetchMyIP((error, ip) => {
 //   if (error) {
 //     console.log("It didn't work!" , error);
